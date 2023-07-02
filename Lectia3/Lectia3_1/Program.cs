9d42f1daec0705375ca33180417d794b67d852dc
ataras <ataras@LT-ATARAS>
2023-07-02 19:45:23 +0300

    Lesson 3 Recording

--- Lectia3/Lectia3_1/Program.cs
+++ Lectia3/Lectia3_1/Program.cs
@@ -2,10 +2,22 @@
 
 namespace Lectia3_1
 {
+    // [modificator de acces]
     internal class Program
     {
+        public static void Hello(string name)
+        {
+            Console.WriteLine("Hello, " + name);
+        }
+
+        // static [tip return | void] [name of of the method in Pascal Case | MainMethod]
+        // (list of input parameters)
         public static void Main()
         {
+            Hello("Dorin");
+            Hello("Alex");
+            Hello("Cristian");
+                   
             // 1. Preluam de la tastatura si convertim in numar prima valoare
             // introdus de catre utilizator in consola.
 
@@ -13,7 +25,7 @@ namespace Lectia3_1
             Console.Write("Introdu, te rog frumos, primul numar: ");
             double PrimulNumar = double.Parse(Console.ReadLine());
 
-            // 2. Preluam de la tastatura si convertim in numar a doua valpoare
+            // 2. Preluam de la tastatura si convertim in numar a doua valoare
             Console.Write("Introdu, te rog frumos, al doilea numar: ");
             double alDoileaNumar = double.Parse(Console.ReadLine());
 
