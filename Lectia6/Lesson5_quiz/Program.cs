39bc9aa3f2057f4ca4f66099f342757c5bb811fc
ataras <ataras@LT-ATARAS>
2023-07-02 21:10:20 +0300

    Lesson 6 Recording

---
+++ Lectia6/Lesson5_quiz/Program.cs
@@ -0,0 +1,24 @@
+﻿struct Employee
+{
+    public string name;
+    public double salary;
+}
+
+public class Program
+{
+    static void Main()
+    {
+        var angajat1 = new Employee
+        {
+            name = "Ion",
+            salary = 2500
+        };
+
+        var angajat2 = angajat1;
+
+        angajat2.salary = 3000;
+
+        Console.WriteLine(angajat1.salary);
+        Console.WriteLine(angajat2.salary);
+    }
+}
