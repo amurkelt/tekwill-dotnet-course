2d104d7df1d4955b198593b1da588eca0b63bd6e
ataras <ataras@LT-ATARAS>
2023-07-02 14:36:33 +0300

    Added Lesson 2 HW

--- Lectia3/Lectia3/BasicConventions/Program.cs
+++ Lectia3/Lectia3/BasicConventions/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Exemplu
+{
+    public class MyClasa
+    {
+        public string Nume;
+
+        private int var1;
+
+        public void AfiseazaNumar(int numar)
+        {
+            if (numar > 0)
+                Console.WriteLine("Numarul este pozitiv.");
+            else
+                Console.WriteLine("Numarul este negativ.");
+        }
+
+        public static void CalculeazaProdus(int a, int b)
+        {
+            int produs = a * b;
+            Console.WriteLine("Produsul numerelor este: " + produs);
+        }
+
+        public void CalculeazaSuma(int a, int b)
+        {
+            int suma = a + b;
+            Console.WriteLine("Suma numerelor este: " + suma);
+        }
+
+
+    }
+    //home cotroller
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClasa obj = new MyClasa();
+            obj.AfiseazaNumar(10);
+            obj.Nume = "Exemplu";
+            Console.WriteLine(obj.Nume);
+            obj.CalculeazaSuma(5, 3);
+            MyClasa.CalculeazaProdus(2, 4);
+            Console.ReadLine();
+        }
+    }
+}
