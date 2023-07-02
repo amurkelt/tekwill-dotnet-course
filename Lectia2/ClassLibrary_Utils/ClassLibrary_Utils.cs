efb5312fac9ff193b4490ed29db93e7e85b6c066
ataras <ataras@LT-ATARAS>
2023-07-02 19:12:35 +0300

    Lesson 2 Recording

---
+++ Lectia2/ClassLibrary_Utils/ClassLibrary_Utils.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassLibrary_Utils
+{
+    public static class Utils
+    {
+        public static string GetMessage() 
+        {
+            return "Hello world from Utils class library project";
+        }
+    }
+}
---
+++ Lectia2/HelloTekwill/HelloTekwill.cs
@@ -0,0 +1,7 @@
+﻿using ClassLibrary_Utils;
+
+Console.WriteLine(ClassLibrary_Utils.Utils.GetMessage());
+Console.WriteLine("1234");
+Console.WriteLine("4444");
+
+Console.ReadLine();
