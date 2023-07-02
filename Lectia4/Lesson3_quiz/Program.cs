2d5710d2dfc084fa209f7447c8f07d49600627bc
ataras <ataras@LT-ATARAS>
2023-07-02 20:40:02 +0300

    HW repositioned: Prev lesson --> Beginning of next lesson

---
+++ Lectia4/Lesson3_quiz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HelloTekwill
+{
+    class Program2
+    {   
+        static void Main()
+        {
+        Console.WriteLine("Introduceți trei numere:");
+
+        // Citirea primului număr
+        Console.Write("Primul numar:");
+        int numar1 = int.Parse(Console.ReadLine());
+
+        // Citirea celui de-al doilea număr
+        Console.Write("Al doilea numar: ");
+        int numar2 = int.Parse(Console.ReadLine());
+
+        // Citirea celui de-al treilea număr
+        Console.Write("Al 3-lea numar: ");
+        int numar3 = int.Parse(Console.ReadLine());
+
+        // Calcularea sumei
+        int suma = numar1 + numar2 + numar3;
+
+        // Afișarea rezultatului
+        Console.WriteLine("Suma numerelor: {0},{1},{2} este {3}", numar1, numar2, numar3, suma);
+    }
+}
+}
