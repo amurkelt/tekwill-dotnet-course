a4620bf427b553827513f678ec05326ed2ecd09f
ataras <ataras@LT-ATARAS>
2023-07-07 01:18:17 +0300

    HW 1 and 2

---
+++ Lectia9/Homework_1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Tema 1: Utilizare for
+// 1.	Creați un program care să afișeze toate numerele pare într-un interval dat.
+// 2.	Solicitați utilizatorului să introducă un număr de început și un număr de sfârșit al intervalului.
+// 3.	Utilizați o buclă for pentru a itera prin toate numerele din interval.
+// 4.	În fiecare iterație, verificați dacă numărul este par și, dacă este, afișați-l la ecran.
+class Program
+{
+    static void Main()
+    {
+        int arrayStart = 0;
+        int arrayEnd = 0;
+        int intervalLength = 0;
+
+        do
+        {
+            Console.WriteLine("Introdu un numar de inceput al intervalului");
+            arrayStart = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introdu un numar de sfarsit al intervalului");
+            arrayEnd = int.Parse(Console.ReadLine());                      
+        }
+        while (arrayStart >= arrayEnd);
+
+        intervalLength = arrayEnd - arrayStart + 1;
+        int[] interval = new int[intervalLength];
+        Console.WriteLine("Urmatoare numere in intervalul dat sunt pare: ");
+
+        for (int i = 0; i < intervalLength; i++)
+        {
+            interval[i] = arrayStart + i;
+
+            if (interval[i] % 2 == 0)
+            {
+                Console.Write($"{interval[i]} ");
+            }
+        }
+
+    }
+}
---
+++ Lectia9/Homework_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Tema 2: Utilizare while
+// 1.	Creați un program care să solicite utilizatorului să introducă numere de la tastatură până când introducerea valorii 0.
+// 2.	Utilizați o buclă while pentru a verifica continuu valoarea introdusă și a afișa un mesaj corespunzător în funcție de valoarea introdusă.
+// 3.	Întrerupeți bucla atunci când utilizatorul introduce valoarea 0 și afișați un mesaj de încheiere.
+using System.Diagnostics.Metrics;
+
+class Program
+{
+    static void Main()
+    {
+        int breakpoint = 1;
+
+        while (breakpoint != 0)
+        {
+            Console.WriteLine("Introduceti numere de la tastatura \nValoarea 0 o sa incheie program");
+            breakpoint = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            if (breakpoint == 0)
+            {
+                Console.WriteLine("Mater has given Dobby a sock. Master has presented Dobby with clothes. Dobby is free.");
+            }
+        }
+    }
+}
