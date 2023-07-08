f2a1d4cebefce28e16c0a87434bd62ee16506053
ataras <ataras.md@gmail.com>
2023-07-08 10:07:51 +0300

    Lesson 9 Recording

--- Lectia9/Lectia9_1/Program.cs
+++ Lectia9/Lectia9_1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Pr
+{
+    static void Main()
+    {
+        int startInterval = 3;
+        int endInterval = 30;
+
+        for (int i = startInterval; i <= endInterval; i++)
+        {
+            if (i % 2 == 0) Console.WriteLine(i);
+        }
+    }
+}
