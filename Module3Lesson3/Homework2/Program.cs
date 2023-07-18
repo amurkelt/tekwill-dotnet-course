488e735132fdecbb7a052ca7c01419d1fedebdaf
ataras <ataras.md@gmail.com>
2023-07-18 16:35:08 +0300

    init-hw-lesson13

---
+++ Module3Lesson3/Homework2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Tema: Utilizarea proprietăților și metodelor într-o clasă.
+//1.	Creați o clasă numită Carte care să reprezinte informații despre o carte.
+//2.	Adăugați următoarele proprietăți la clasa Carte:
+//•	Titlu - pentru a reține titlul cărții
+//•	Autor - pentru a reține numele autorului cărții
+//•	AnPublicare - pentru a reține anul de publicare al cărții
+//•	NumarPagini - pentru a reține numărul de pagini al cărții
+//3.	Implementați un constructor cu parametri în clasa Carte care să primească argumente pentru titlu, autor, anul de publicare și numărul de pagini și să le atribuie proprietăților corespunzătoare.
+//4.	Adăugați o metodă numită AfisareDetalii în clasa Carte care să afișeze toate detaliile cărții.
+//5.	Testați clasa Carte în funcția Main a programului.
+//•	Creați un obiect de tip Carte, specificând valorile pentru titlu, autor, anul de publicare și numărul de pagini.
+//•	Apelați metoda AfisareDetalii pentru a afișa detaliile cărții.
---
+++ Module3Lesson3/Homework3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Tema: Utilizarea constructorului de copiere într-o clasă.
+//1.	Creați o clasă numită Student care să reprezinte informații despre un student.
+//2.	Adăugați următoarele proprietăți la clasa Student:
+//•	Nume - pentru a reține numele studentului
+//•	Varsta - pentru a reține vârsta studentului
+//•	Specializare - pentru a reține specializarea studentului
+//3.	Implementați un constructor cu parametri în clasa Student care să primească argumente pentru nume, vârstă și specializare și să le atribuie proprietăților corespunzătoare.
+//4.	Implementați un constructor de copiere în clasa Student care să primească un obiect Student și să creeze o copie a acestuia.
+//5.	Adăugați o metodă numită AfisareDetalii în clasa Student care să afișeze toate detaliile studentului.
+//6.	Testați constructorul de copiere în funcția Main a programului.
+//•	Creați un obiect de tip Student și atribuiți-i valorile pentru nume, vârstă și specializare.
+//•	Creați un nou obiect de tip Student utilizând constructorul de copiere și atribuiți-i obiectului anterior.
+//•	Modificați valorile proprietăților obiectului original și apoi afișați detaliile ambelor obiecte folosind metoda AfisareDetalii.
