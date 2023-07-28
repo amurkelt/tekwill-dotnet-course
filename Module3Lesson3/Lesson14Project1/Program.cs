5a93a9377d72d6d093c7e9d695541abe2defa262
ataras <ataras.md@gmail.com>
2023-07-28 16:28:43 +0300

    recording-lesson14

--- Module3Lesson3/Lesson14Project1/Program.cs
+++ Module3Lesson3/Lesson14Project1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class UserService
+{
+    public static string Number = "001";
+
+    private List<string> Users { get; set; }
+
+    public void AddUser()
+    {
+        // ...
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+
+        var userService1 = new UserService();
+        userService1.AddUser();
+
+        Console.WriteLine(UserService.Number);
+
+        var userService2 = new UserService();
+    }
+}
