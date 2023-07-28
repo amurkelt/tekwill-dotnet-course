5d82ad16ab281800f61440b55a06e65de90cc26e
ataras <ataras.md@gmail.com>
2023-07-28 17:11:54 +0300

    Recording Lesson 15

---
+++ Module3Lesson4/Lesson15Project2/Profesor.cs
@@ -0,0 +1,12 @@
+﻿using Lesson15Project2_class_library;
+
+namespace Lectia16_2
+{
+    public class Profesor : Persoana
+    {
+        public void Informatii()
+        {
+            Console.WriteLine(ProtectedInternal);
+        }
+    }
+}
---
+++ Module3Lesson4/Lesson15Project4_logger/ILogger.cs
@@ -0,0 +1,7 @@
+﻿namespace Lesson15Project4_logger
+{
+    public interface ILogger
+    {
+        public void LogInfo(string info);
+    }
+}
